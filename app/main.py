d45db45407994f1617576a16dd98f983d468fd2b
Jonn <jonnchristophersalvador@gmail.com>
2022-05-24 02:17:44 +0800

    [UPDATE] Update terms of service

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ root  = Tk()
 # to change window title
 root.title('Savings Tracker')
 # to set window size
-root.geometry("700x350")
+root.geometry("740x390") # orig window 700x350
 
 def destroy(m):
     tkinter.messagebox.showinfo('Terms of Service', m)
@@ -25,10 +25,10 @@ try:
             x_tos = x['tos']
     # to create label
     _w_s_01 = Label(root,text="").pack(side=TOP,pady=20) # blank label to serve as divider
-    _title = Label(root,text=x_tos['title'],font=("Arial",17,"bold"),).pack(side=TOP,pady=12)
-    _tos = Label(root,text=x_tos['tos'],font=("Arial",10),).pack(side=TOP,pady=1)
-    _tos = Label(root,text=x_tos['tos_text'],font=("Arial",8),).pack(side=TOP,pady=0)
-    _accept = Button(root,text="ACCEPT TERMS OF SERVICE",font=("Arial", 9),command=lambda m="You accepted terms of service": destroy(m)).pack(side=TOP,pady=25) 
+    _title = Label(root,text=x_tos['title'],font=("Arial",18,"bold"),).pack(side=TOP,pady=12) # orig font 17
+    _tos = Label(root,text=x_tos['tos'],font=("Arial",10),).pack(side=TOP,pady=1) # orig font 10
+    _tos = Label(root,text=x_tos['tos_text'],font=("Arial",10),).pack(side=TOP,pady=1) # orig font 8 : pady 0
+    _accept = Button(root,text="ACCEPT TERMS OF SERVICE",font=("Arial", 10),command=lambda m="You accepted terms of service": destroy(m)).pack(side=TOP,pady=30) # orig font 9 : pady 30
 except:
     for x in range(0,5):
         print(" ERROR: TOS.JSON COULDN'T BE FOUND. PLEASE CHECK IF EXIST IN APP FOLDER")
